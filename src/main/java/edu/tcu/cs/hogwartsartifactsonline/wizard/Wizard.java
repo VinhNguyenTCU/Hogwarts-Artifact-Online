a432a9f50d04c503102fc1c166358d600cfaabfa
Vinh Nguyen <vinh29012005@gmail.com>
2025-03-24 20:32:18 -0500

    Populating the H2 Database with Test Data

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/Wizard.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/Wizard.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,7 +19,7 @@ public class Wizard implements Serializable {
     private String name;
 
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "owner")
-    private List<Artifact> artifacts;
+    private List<Artifact> artifacts = new ArrayList<>();
 
     public Wizard() {
     }
